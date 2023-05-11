a929da3298b3d28eaa9cc811c98595b9b78f3f71
Kamogelo Khumalo <KamoKhumalo04@gmail.com>
2023-05-11 17:55:11 +0200

    Changed email

--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -5,7 +5,7 @@ sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 async function sendEmail(req, res) {
   try {
     await sendgrid.send({
-      to: "projects.clearview@mweb.co.za", // Your email where you'll receive emails
+      to: "mvavana@gmail.com", // Your email where you'll receive emails
       from: "customerinqury@gmail.com", // your website email address here
       subject: `[Customer requesting quotation from website] : ${req.body.subject}`,
       html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
